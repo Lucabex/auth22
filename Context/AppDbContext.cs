3c2d7092d6a12f456cf6406bdab8a996126674d0
Luca Bercioux <Lucabex@gmail.com>
2026-09-15 23:15:14 +0100

    Add Dbcontext

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,2 +1,12 @@
+using auth22.Models;
+using Microsoft.EntityFrameworkCore;
 namespace auth22.Context;
 
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<User>User{get;set;}
+}
