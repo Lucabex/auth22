d53c07306e33c86fd4756fdbbf908eda30bf3ad0
Luca Bercioux <Lucabex@gmail.com>
2026-09-16 11:30:14 +0100

    create token generator

--- Services/JwtService.cs
+++ Services/JwtService.cs
@@ -1,8 +1,44 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using auth22.Models;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualBasic;
+
 namespace auth22.Services;
 public class JwtService
 {
-    public JwtService()
+    private readonly IConfiguration _config;
+    private readonly SymmetricSecurityKey _key;
+
+    public JwtService(IConfiguration config,SymmetricSecurityKey key)
     {
-        
+        _config=config;
+        var secretKey = _config["JwtService:SecretKey"];
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!));
+    }
+
+    public string GenerateToken(User user)
+    {      
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+            new Claim(ClaimTypes.Name,user.UserName ),
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Audience = _config["JwtSettings:"],
+            Issuer = _config["JwtSettings:Issuer"],
+            Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_config["JwtSettings:ExpInMinutes"])),
+            SigningCredentials = new SigningCredentials(_key,SecurityAlgorithms.HmacSha256)
+        };
+
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.CreateToken(tokenDescriptor);
+        return handler.WriteToken(token);
     }
 }
